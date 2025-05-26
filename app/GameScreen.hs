ec214040e8312dbf3a3adee024de6965ad99bd3b
Toma Sasaki <netst915@gmail.com>
2025-05-27 00:27:03 +0900

    use svg files

--- app/GameScreen.hs
+++ app/GameScreen.hs
@@ -2,24 +2,23 @@ module GameScreen (renderGameScreen) where
 
 import           Miso
 import           Miso.String          (ms)
-import           Miso.Svg             (g_, svg_, transform_)
 
 import           GameState
 import           GameState.Difficulty (Difficulties (..), Difficulty)
-import           Text.Printf          (printf)
-import           Textures             (Texture (..), Textures (..))
 
 renderGameScreen :: Difficulty -> GameState -> View GameAction
 renderGameScreen difficulty state = do
     let screenWidth = width $ difficulty
         screenHeight = height $ difficulty
 
-    svg_ [width_ (ms $ 32 * screenWidth), height_ (ms $ 32 * screenHeight)] $
-        flip map [(x, y) | x <- [1..screenWidth], y <- [1..screenHeight]] $ \(x, y) -> do
-            let cell = Cell x y
+    div_ [class_ "gameScreen"] $
+        flip map [1..screenHeight] $ \y ->
+            div_ [class_ "gameRow"] $
+                flip map [1..screenWidth] $ \x -> do
+                    let cell = Cell x y
+                        cellClasses = ["gameCell"] ++
+                            case hasCellMine cell state of
+                                False -> ["closedCell"]
+                                True  -> ["openedCellWithMine"]
 
-            g_ [transform_ (ms (printf "translate(%d,%d)" (32 * (x - 1)) (32 * (y - 1)) :: String)), onClick (CellClicked cell)] $
-                textureElements $
-                    case hasCellMine cell state of
-                        False -> OpenedCell
-                        True  -> OpenedCellWithMine
+                    div_ [class_ (ms $ unwords cellClasses), onClick (CellClicked cell)] []
