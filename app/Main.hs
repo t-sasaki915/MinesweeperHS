ee4a5c25fbe380cfa515fbac0e286e5424208a4a
Toma Sasaki <netst915@gmail.com>
2025-05-27 10:24:11 +0900

    update main

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,24 @@ import           GHC.JS.Prim
 foreign import javascript "((message) => console.log(message))"
     consoleLog :: JSVal -> IO ()
 
+foreign import javascript "((elementName) => document.createElement(elementName))"
+    createElement :: JSVal -> IO JSVal
+
+foreign import javascript "((element, newId) => element.id = newId)"
+    setElementId :: JSVal -> JSVal -> IO ()
+
+foreign import javascript "((element, newClasses) => element.className = newClasses)"
+    setElementClasses :: JSVal -> JSVal -> IO ()
+
+foreign import javascript "((element) => document.body.appendChild(element))"
+    appendChildToDocument :: JSVal -> IO ()
+
 main :: IO ()
-main = consoleLog (toJSString "TEST")
+main = do
+    element <- createElement (toJSString "div")
+    setElementId element (toJSString "asdf")
+    setElementClasses element (toJSString "gameCell closedCell")
+
+    appendChildToDocument element
+
+    consoleLog (toJSString "TEST")
